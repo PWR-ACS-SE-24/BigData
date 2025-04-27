ef6ed1b9481584ffe45e9b93ddacbc2d4daca26e
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-04-27 20:47:26 +0200

    Add loader for non-dynamic sources

--- zad6/loader/main.py
+++ zad6/loader/main.py
@@ -1,5 +1,9 @@
-from hdfs import InsecureClient
+import math
+import os
 from datetime import datetime
+from time import time_ns
+import duckdb
+from hdfs import InsecureClient
 
 client = InsecureClient("http://10.0.2.3:9870", user="root")
 
@@ -13,19 +17,39 @@ def log(message: str) -> None:
     client.write("/logs.txt", message, append=True, encoding="utf-8")
 
 def upload_input_file(name: str) -> None:
+    size = os.path.getsize(f"/root/data/{name}")
     log(f"{name} uploading...")
+    start = time_ns()
     try:
         client.upload(f"/input/{name}", f"/root/data/{name}")
     except Exception as e:
         log(f"{name} upload failed !!!")
         log(e)
         return
+    end = time_ns()
     try:
         client.set_replication(f"/input/{name}", 4)
     except Exception as e:
         log(f"{name} replication failed !!!")
         log(e)
         return
-    log(f"{name} uploaded successfully")
+    log(f"{name} uploaded successfully in {(end - start) / 1_000_000:.3f} ms, {math.ceil(size / (1024 * 1024)):.3f} MB")
+    log(f"{name} status: {client.status(f"/input/{name}")}")
 
-upload_input_file("cities.csv")
+def convert_to_parquet(name: str) -> None:
+    pq_name = os.path.splitext(name)[0] + ".parquet"
+    if os.path.exists(f"/root/data/{pq_name}"): # TODO
+        log(f"{name} already converted to parquet, skipping...")
+        return
+    log(f"{name} converting to parquet...")
+    duckdb.sql(f"""COPY (SELECT * FROM '/root/data/{name}') TO '/root/data/{pq_name}' (FORMAT PARQUET)""")
+    log(f"{name} converted to parquet successfully")
+
+convert_to_parquet("charts.csv")
+convert_to_parquet("cities.csv")
+convert_to_parquet("WDIData.csv")
+
+upload_input_file("charts.parquet")
+upload_input_file("cities.parquet")
+upload_input_file("daily_weather.parquet")
+upload_input_file("WDIData.parquet")
