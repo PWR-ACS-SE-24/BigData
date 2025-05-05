0c99a02f45742c83ababe956356d733800636bca
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-05-05 17:02:50 +0200

    Incremental artist and genre fetching

--- zad6/loader/main.py
+++ zad6/loader/main.py
@@ -73,47 +73,57 @@ def upload_input_file(name: str) -> None:
 
 def fetch_artists_from_tracks() -> None:
     log("Fetching artists from tracks...")
-    duckdb.sql("CREATE TABLE IF NOT EXISTS artists_from_tracks (track_id VARCHAR, artist_id VARCHAR)")
-    duckdb.sql("TRUNCATE artists_from_tracks")
-    cursor = duckdb.sql("SELECT DISTINCT url FROM '/root/data/charts.csv'")
-    while True:
-        row = cursor.fetchone()
-        if row is None:
-            break
-        track_id = row[0].split("/")[-1]
-        response = requests.get(f"http://localhost:1234/v1/tracks/{track_id}")
-        if response.status_code != 200:
-            log(f"{track_id} {response.status_code}", file_only=True)
-            continue
-        data = response.json()
-        log(f"{track_id} {response.status_code} {data}", file_only=True)
-        for artist in data["artists"]:
-            duckdb.sql(f"INSERT INTO artists_from_tracks VALUES (?, ?)", params=(track_id, artist["id"]))
-    duckdb.sql("COPY artists_from_tracks TO '/root/data/artists_from_tracks.csv' (FORMAT CSV, HEADER TRUE)")
-    duckdb.sql("DROP TABLE artists_from_tracks")
-    log("Artists from tracks fetched successfully")
+    with duckdb.connect() as reader, duckdb.connect() as writer:
+        writer.sql("CREATE TABLE IF NOT EXISTS artists_from_tracks (track_id VARCHAR, artist_id VARCHAR)")
+        writer.sql("TRUNCATE artists_from_tracks")
+        try:
+            writer.sql("COPY artists_from_tracks FROM '/root/data/artists_from_tracks.csv' (FORMAT CSV, HEADER TRUE)")
+        except:
+            pass
+        track_ids = set(row[0] for row in writer.sql("SELECT DISTINCT track_id FROM artists_from_tracks").fetchall())
+        cursor = reader.sql("SELECT DISTINCT url FROM '/root/data/charts.csv'")
+        while (row := cursor.fetchone()) is not None:
+            track_id = row[0].split("/")[-1]
+            if track_id in track_ids:
+                continue
+            response = requests.get(f"http://localhost:1234/v1/tracks/{track_id}")
+            if response.status_code != 200:
+                log(f"{track_id} {response.status_code}", file_only=True)
+                continue
+            data = response.json()
+            log(f"{track_id} {response.status_code} {data}", file_only=True)
+            for artist in data["artists"]:
+                writer.sql(f"INSERT INTO artists_from_tracks VALUES (?, ?)", params=(track_id, artist["id"]))
+        writer.sql("COPY artists_from_tracks TO '/root/data/artists_from_tracks.csv' (FORMAT CSV, HEADER TRUE)")
+        writer.sql("DROP TABLE artists_from_tracks")
+        log("Artists from tracks fetched successfully")
 
 def fetch_genres_from_artists() -> None:
     log("Fetching genres from artists...")
-    duckdb.sql("CREATE TABLE IF NOT EXISTS genres_from_artists (artist_id VARCHAR, genre VARCHAR)")
-    duckdb.sql("TRUNCATE genres_from_artists")
-    cursor = duckdb.sql("SELECT DISTINCT artist_id FROM '/root/data/artists_from_tracks.csv'")
-    while True:
-        row = cursor.fetchone()
-        if row is None:
-            break
-        artist_id = row[0]
-        response = requests.get(f"http://localhost:1234/v1/artists/{artist_id}")
-        if response.status_code != 200:
-            log(f"{artist_id} {response.status_code}", file_only=True)
-            continue
-        data = response.json()
-        log(f"{artist_id} {response.status_code} {data}", file_only=True)
-        for genre in data["genres"]:
-            duckdb.sql(f"INSERT INTO genres_from_artists VALUES (?, ?)", params=(artist_id, genre))
-    duckdb.sql("COPY genres_from_artists TO '/root/data/genres_from_artists.csv' (FORMAT CSV, HEADER TRUE)")
-    duckdb.sql("DROP TABLE genres_from_artists")
-    log("Genres from artists fetched successfully")
+    with duckdb.connect() as reader, duckdb.connect() as writer:
+        writer.sql("CREATE TABLE IF NOT EXISTS genres_from_artists (artist_id VARCHAR, genre VARCHAR)")
+        writer.sql("TRUNCATE genres_from_artists")
+        try:
+            writer.sql("COPY genres_from_artists FROM '/root/data/genres_from_artists.csv' (FORMAT CSV, HEADER TRUE)")
+        except:
+            pass
+        artist_ids = set(row[0] for row in writer.sql("SELECT DISTINCT artist_id FROM genres_from_artists").fetchall())
+        cursor = reader.sql("SELECT DISTINCT artist_id FROM '/root/data/artists_from_tracks.csv'")
+        while (row := cursor.fetchone()) is not None:
+            artist_id = row[0]
+            if artist_id in artist_ids:
+                continue
+            response = requests.get(f"http://localhost:1234/v1/artists/{artist_id}")
+            if response.status_code != 200:
+                log(f"{artist_id} {response.status_code}", file_only=True)
+                continue
+            data = response.json()
+            log(f"{artist_id} {response.status_code} {data}", file_only=True)
+            for genre in data["genres"]:
+                writer.sql(f"INSERT INTO genres_from_artists VALUES (?, ?)", params=(artist_id, genre))
+        writer.sql("COPY genres_from_artists TO '/root/data/genres_from_artists.csv' (FORMAT CSV, HEADER TRUE)")
+        writer.sql("DROP TABLE genres_from_artists")
+        log("Genres from artists fetched successfully")
 
 if __name__ == "__main__":
     initialize()
