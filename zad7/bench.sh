52036913169bcd30967cfe465e530c65e89c8268
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-05-06 00:13:37 +0200

    Add more benches

--- zad7/bench.sh
+++ zad7/bench.sh
@@ -8,5 +8,7 @@ yarn jar zad7-all.jar Benchmark 1 2 128 > bench_1_2_128.txt
 yarn jar zad7-all.jar Benchmark 2 3 128 > bench_2_3_128.txt
 yarn jar zad7-all.jar Benchmark 3 3 128 > bench_3_3_128.txt
 
+yarn jar zad7-all.jar Benchmark 1 3 32 > bench_1_3_32.txt
+yarn jar zad7-all.jar Benchmark 1 3 64 > bench_1_3_64.txt
 yarn jar zad7-all.jar Benchmark 1 3 192 > bench_1_3_192.txt
 yarn jar zad7-all.jar Benchmark 1 3 256 > bench_1_3_256.txt
