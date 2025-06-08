37b21efdcaa75ec6394e49072ed8a73a3ab36ba4
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-06-08 23:03:53 +0200

    Update queries

--- zad10/notebooks/zad11.py
+++ zad10/notebooks/zad11.py
@@ -1,8 +1,11 @@
+import os
+import time
 from typing import LiteralString
 from pyspark.sql import DataFrame, SparkSession
 from IPython.display import display
 
 def connect() -> SparkSession:
+    print("Connecting to Spark...")
     spark = SparkSession.builder\
         .appName("Zad11")\
         .master("yarn")\
@@ -13,20 +16,26 @@ def connect() -> SparkSession:
     return spark
 
 
-def load_table(spark: SparkSession, name: str, *, header: list[str] | None = None) -> DataFrame:
+def load_table(spark: SparkSession, path: LiteralString, *, header: list[str] | None = None) -> DataFrame:
+    name = os.path.basename(path).split('.')[0]
+    print(f"Loading '{path}' into table '{name}'...")
     df = spark.read\
         .option("header", "true" if header is None else "false")\
-        .csv(f"../../input/{name}.csv")
+        .csv(path)
     if header is not None:
         df = df.toDF(*header)
     df.createOrReplaceTempView(name)
     return df
 
 
-def process(spark: SparkSession, name: str, sql: LiteralString) -> None:
+def process(spark: SparkSession, name: LiteralString, sql: LiteralString) -> None:
+    print(f"Processing query and saving to '/{name}/*'...")
+    start_time = time.time_ns()
     df = spark.sql(sql)
     df.write\
       .mode("overwrite")\
       .option("header", "true")\
-      .csv(f"../../{name}.csv")
+      .csv(f"/{name}")
+    end_time = time.time_ns()
+    print(f"Execution time: {(end_time - start_time) / 1e9:.3f} seconds")
     df.show()
